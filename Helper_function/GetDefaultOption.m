a79653dc8525c92438f9e0a383b9a4a43d81c3f5
xcsun0 <84862833+xcsun0@users.noreply.github.com>
2026-06-30 09:18:23 -0700

    Update GetDefaultOption.m

--- Helper_function/GetDefaultOption.m
+++ Helper_function/GetDefaultOption.m
@@ -2,33 +2,35 @@ function option=GetDefaultOption(varagin)
 
     % Soma-print 2D parameters
 
+    option.pixellength=1.31; % Critical parameters, pixel size for in vivo image (um/pixel) 
+
     option.nitermax=10; % Maximum number of iterations
     option.nitermin=3;  % Minimum number of iterations
     option.n_vec1=15; % 1st iteration
     option.n_vec2=10; % 2nd iteration
-    option.n_vec3=option.n_vec2;%10; % 3rd iteration
-    option.p_sum1=0.66;
-    option.p_sum2=1;
+    option.n_vec3=[];%10; % 3rd iteration
+    option.p_sum1=0.66; % Percent of vectors used to compute Soma-print score, 1st round 
+    option.p_sum2=1; Percent of vectors used to compute Soma-print score, since 2nd round
     option.lr1st=0.05; % LR cutoff for 1st round 
     option.lr2nd=0.05; % LR cutoff from 2nd round 
     option.foldexvivo=1;  % Ratio of ex vivo cell / in vivo cells
     option.lambda=0.001; % GMM model parameter
     option.gmmfilter=0; % GMM model parameter: critical for nosiy data with many unmatched cells 
     option.method=1; % GMM model parameter: Method 
-
-    option.pixellength=1.31; % Critical parameters 
-    option.sigma=100;% in um
-    option.sigma2=100;% in um
-    option.anchorsigma=250; % in um
+    option.sigma=100;% Distance penalty in um, 1st round 
+    option.sigma2=100;% Distance penalty in um, since 2nd round
+    option.anchorsigma=250; % Anchor cell spread penalty in um
 
      % Soma-print 3D parameters
     if exist('varagin')==1 & varagin== 3;
+
+        option.pixellength=0.66; % Critical parameters 
 
         option.nitermax=10; % Maximum number of iterations
         option.nitermin=3;  % Minimum number of iterations
         option.n_vec1=15; % 1st iteration
         option.n_vec2=10; % 2nd iteration
-        option.n_vec3=option.n_vec2;%10; % 3rd iteration
+        option.n_vec3=[];%10; % 3rd iteration
         option.p_sum1=0.66;
         option.p_sum2=1;
         option.lr1st=0.05; % LR cutoff for 1st round 
@@ -36,36 +38,11 @@ function option=GetDefaultOption(varagin)
         option.foldexvivo=1;  % Ratio of ex vivo cell / in vivo cells
         option.lambda=1; % GMM model parameter
         option.gmmfilter=0; % GMM model parameter: critical for nosiy data with many unmatched cells 
-        option.method=1; % GMM model parameter: Method 
-    
-        option.pixellength=0.66; % Critical parameters 
+        option.method=1; % GMM model parameter: Method   
         option.sigma=100;% in um
         option.sigma2=150;% in um
         option.anchorsigma=75; % in um (%LFOV: 150 um)
-
         
     end
 
-    if exist('varagin')==1 & varagin==4;
-
-        option.nitermax=10; % Maximum number of iterations
-        option.nitermin=5;  % Minimum number of iterations
-        option.n_vec1=15; % 1st iteration
-        option.n_vec2=5; % 2nd iteration
-        option.n_vec3=10; % 3rd iteration
-        option.p_sum1=0.66;
-        option.p_sum2=1;
-        option.lr1st=0.2; % LR cutoff for 1st round 
-        option.lr2nd=0.05; % LR cutoff from 2nd round 
-        option.foldexvivo=1;  % Ratio of ex vivo cell / in vivo cells
-        option.lambda=1; % GMM model parameter
-        option.gmmfilter=0; % GMM model parameter: critical for nosiy data with many unmatched cells 
-        option.method=1; % GMM model parameter: Method 
-    
-        option.pixellength=0.75; % Critical parameters 
-        option.sigma=50;% in um
-        option.sigma2=50;% in um
-        option.anchorsigma=50; % in um
-
-    end
 end
